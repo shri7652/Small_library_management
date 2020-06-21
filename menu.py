669a2eb89d8746f06a9338b07f2f5fca5407072b
ranju <ranju1729@gmail.com>
2020-06-21 16:06:11 +0530

    There is some issue with menu driving, need to identify and fix

--- menu.py
+++ menu.py
@@ -5,8 +5,7 @@ from utils import *
 def theMainMenu(user_type):
     """Takes in the user_type and sends the main menu screen accordingly"""
     if user_type == 'admin':
-        menuFunc = {"1" : "userManagement()", "2": "bookShelfManagement()", "3" : "bookShelfBrowse()", "4": "updatePassword()",
-                    "5" : "exit"}
+
         menu = """
         1. User Management
         2. Book Shelf Management
@@ -14,6 +13,8 @@ def theMainMenu(user_type):
         4. update password
         5. Quit
         """
+
+
 
     else:
         menuFunc = {"1" : "bookShelfBrowse()", "2": "updatePassword()", "3" : "exit"}
@@ -23,9 +24,36 @@ def theMainMenu(user_type):
         2 update password
         3 quit
         """
+
+    menuFunc = [{1 : userManagement(), 2: "bookShelfManagement()", 3 : "bookShelfBrowse()", 4: "updatePassword()",  5 : "exit"}]
     clear()
     print("*******Welcome to Small Library Management - Main Menu******")
     print(menu)
+    choice = int(str(input("Enter your choice")).strip())
+
+    if menuFunc[0][choice] == "exit":
+        print("quitting")
+        time.sleep(3)
+        sys.exit(0)
+    else:
+        print(menuFunc[0][choice])
+        time.sleep(3)
+
+
+
+
+def userManagement():
+    userMenu = """
+    1 Add User
+    2 Remove User
+    3 Go back to main menu
+    4 quit
+    """
+    menuFunc = {"1" : addUser(), "2" : removeUser(), "3" : theMainMenu(Session.user_type), "4" : "exit"}
+
+    clear()
+    print("*******Welcome to Small Library Management - User Management******")
+    print(userMenu)
     choice = str(input("Enter your choice")).strip()
 
     if menuFunc[choice] == "exit":
@@ -33,16 +61,7 @@ def theMainMenu(user_type):
         time.sleep(3)
         sys.exit(0)
     else:
-        print(menuFunc[choice])
+        menuFunc[choice]
         time.sleep(3)
 
-
-
-
-
-
-
-
-
-#def userManagement():
 
